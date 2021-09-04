445a17a8f52d897920f543eca906acebc21f5225
shaochenheng <1010716589@qq.com>
2021-09-04 21:11:18 +0800

    use QWebChannel to call python functions and add method to add/remove feed_link

--- src/components/feedsList.vue
+++ src/components/feedsList.vue
@@ -5,37 +5,61 @@
 			color="white" 
 			textColor="black" 
 			title="Feeds List"
-			style="width: 100%;"
-		/>
+			style="width: 100%;"/>
+
 		<div v-for="feed in $store.state.feedsList" 
 		:key="feed.feed_title"
 		class="mu-appbars">
 			<mu-appbar 
 			color="white" 
-			textColor="black" 
-			>
+			textColor="black">
 				{{feed.feed_title}}
 				<mu-button 
 				class="mu-appbars-button"
-				@click="changeCurFeed(feed.feed_title)"
-				>
+				@click="changeCurFeed(feed.feed_title)">
 					check this feed
 				</mu-button>
 			</mu-appbar>
+		</div>
+		<div class="addFeedLinkWidget">
+			<mu-text-field v-model="feedLink" placeholder="Please input feed link......">
+			</mu-text-field><br/>
+			<mu-button @click="addFeedLink(feedLink)">add</mu-button>
 		</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import { QWebChannel } from "qwebchannel";
+
 export default ({
+	
 	data() {
-		return {};	
+		return {
+			feedLink: '',
+		};	
 	},
+	mounted() {
+		window.addFeedLink = this.addFeedLink;
+		window.removeFeedLink = this.removeFeedLink;
+	},
+	created() {
+		// eslint-disable-next-line no-undef
+		new QWebChannel(qt.webChannelTransport, channel => {
+			window.pyobject = channel.objects.pyobject;
+		});
+  },
 	methods: {
 		changeCurFeed(feed_title) {
 			this.$store.commit('changeCurFeed', feed_title);
 			this.$store.commit('changeCurFeedIndex')
+		},
+		addFeedLink() {
+			window.pyobject.add_feed_link(this.feedLink);
+		},
+		removeFeedLink() {
+			window.pyobject.remove_feed_link(this.feedLink);
 		}
 	}
 })
