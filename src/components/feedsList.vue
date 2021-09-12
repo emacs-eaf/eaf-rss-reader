0650a69e56fa2a93486fd7a8accd7ba348f2d727
shaochenheng <1010716589@qq.com>
2021-09-12 23:44:15 +0800

    try to add js_select_next_feed and js_select_prev_feed

--- src/components/feedsList.vue
+++ src/components/feedsList.vue
@@ -6,17 +6,14 @@
 			textColor="black" 
 			title="Feeds List"
 			style="width: 100%;"/>
-
+		<div class="feed-list" ref="feedlist">
 		<div v-for="feed in $store.state.feedsList" 
 		:key="feed.feed_title"
-		class="mu-appbars">
-			<mu-appbar 
-			color="white" 
-			textColor="black">
+		class="mu-appbars"
+		>
+			<mu-appbar color="white" textColor="black">
 				{{feed.feed_title}}
-				<mu-button 
-				class="mu-appbars-button"
-				@click="changeCurFeed(feed.feed_title)">
+				<mu-button  class="mu-appbars-button" @click="changeCurFeed(feed.feed_title)">
 					check this feed
 				</mu-button>
 			</mu-appbar>
@@ -25,6 +22,7 @@
 			<mu-text-field v-model="feedLink" placeholder="Please input feed link......">
 			</mu-text-field><br/>
 			<mu-button @click="addFeedLink(feedLink)">add</mu-button>
+		</div>
 		</div>
 		</div>
 	</div>
@@ -41,6 +39,8 @@ export default ({
 	},
 	mounted() {
 		window.addFeedLink = this.addFeedLink;
+		window.selectNextFeed = this.selectNextFeed;
+		window.selectPrevFeed = this.selectPrevFeed;
 		window.removeFeedLink = this.removeFeedLink;
 	},
 	created() {
@@ -62,9 +62,33 @@ export default ({
 			else{
 				window.pyobject.add_feedlink(new_feedlink);
 			}
+			this.feedLink = '';
 		},
 		removeFeedLink() {
 			window.pyobject.remove_feed_link(this.feedLink);
+		},
+		keepSelectVisible() {
+       this.$refs.feedlist.children[this.currentIndex].scrollIntoViewIfNeeded(false);
+     },
+		selectFeedByIndex(index) {
+       /*
+			 if (index >= this.fileNumber) {
+         this.currentIndex = this.fileNumber - 1;
+       } else if (index <= 0) {
+         this.currentIndex = 0;
+       } else {
+         this.currentIndex = index;
+       }
+
+       this.keepSelectVisible();
+
+       this.updatePreview();*/
+     },
+		selectNextFeed() {
+			this.selectFileByIndex(this.currentIndex + 1);
+		},
+		selectPrevFeed() {
+			this.selectFileByIndex(this.currentIndex - 1);
 		}
 	}
 })
@@ -91,5 +115,5 @@ export default ({
 	position: relative;
 	float: right;
 }
-</style>>
+</style>
 
