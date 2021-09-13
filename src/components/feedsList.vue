24765308f8febbc49f41bbbc46e33d6580865037
shaochenheng <1010716589@qq.com>
2021-09-13 23:11:01 +0800

    add keybinding selectNextFeed selectPrevFeed

--- src/components/feedsList.vue
+++ src/components/feedsList.vue
@@ -1,29 +1,45 @@
 <template>
 	<div class="feedsList">
-		<div class="mu-appbars-title">
-		<mu-appbar 
-			color="white" 
-			textColor="black" 
-			title="Feeds List"
-			style="width: 100%;"/>
-		<div class="feed-list" ref="feedlist">
-		<div v-for="feed in $store.state.feedsList" 
-		:key="feed.feed_title"
-		class="mu-appbars"
-		>
-			<mu-appbar color="white" textColor="black">
-				{{feed.feed_title}}
-				<mu-button  class="mu-appbars-button" @click="changeCurFeed(feed.feed_title)">
-					check this feed
-				</mu-button>
-			</mu-appbar>
-		</div>
-		<div class="addFeedLinkWidget">
-			<mu-text-field v-model="feedLink" placeholder="Please input feed link......">
-			</mu-text-field><br/>
-			<mu-button @click="addFeedLink(feedLink)">add</mu-button>
-		</div>
-		</div>
+		<div class="content">
+			
+			<button @click="selectPrevFeed()">prev</button>
+			<button @click="selectNextFeed()">next</button>
+			
+			<div class="mu-appbars-title">
+				<mu-menu cover>
+					<mu-appbar 
+					color="white" 
+					textColor="black" 
+					style="text-align: center;">
+						Feeds List
+						<mu-button style="float: right;">add feed</mu-button>
+					</mu-appbar>
+
+				<mu-list slot="content">
+				<mu-list-item>
+					<mu-text-field
+					v-model="newFeedLink"
+					placeholder="Please input feed link......"
+					style="margin: auto;">
+					</mu-text-field>
+						<mu-button @click="addFeedLink(newFeedLink)">add</mu-button>
+				</mu-list-item>
+				</mu-list>
+				</mu-menu>
+			</div>
+
+			<div class="feeds-list" ref="feedlist">
+				<div 
+				v-for="feed in $store.state.feedsList" 
+				class="mu-appbars"
+				@click="changeCurFeedByIndex(feed.feed_index)"
+				:key="feed.feed_index">
+					<mu-appbar  textColor="black" :style="{'background':itemBackgroundColor(feed)}">
+						{{feed.feed_title}}
+					</mu-appbar>
+				</div>
+				
+			</div>
 		</div>
 	</div>
 </template>
@@ -34,7 +50,9 @@ import { QWebChannel } from "qwebchannel";
 export default ({
 	data() {
 		return {
-			feedLink: '',
+			newFeedLink: '',
+			backgroundColor:"white",
+			selectColor: "grey",
 		};	
 	},
 	mounted() {
@@ -50,45 +68,47 @@ export default ({
 		});
   },
 	methods: {
-		changeCurFeed(feed_title) {
-			this.$store.commit('changeCurFeed', feed_title);
-			this.$store.commit('changeCurFeedIndex');
+		changeCurFeedByIndex(feed_index) {
+			this.$store.commit('changeCurFeedIndex', feed_index);
 		},
 		addFeedLink(new_feedlink) {
 			const index = this.$store.state.feedsList.findIndex(x => x.feed_link === new_feedlink);
-			if (index != -1){
+			if (index != -1) {
 				alert("Feedlink '"+new_feedlink+"' exists.");
-			}
-			else{
+			} else {
 				window.pyobject.add_feedlink(new_feedlink);
 			}
-			this.feedLink = '';
+			this.newFeedLink = '';
 		},
+		itemBackgroundColor(feed) {
+			if (feed.feed_index == this.$store.state.curFeedIndex) {
+				return this.selectColor;
+			} else {
+				return this.backgroundColor;
+			}
+    },
 		removeFeedLink() {
 			window.pyobject.remove_feed_link(this.feedLink);
 		},
 		keepSelectVisible() {
-       this.$refs.feedlist.children[this.currentIndex].scrollIntoViewIfNeeded(false);
-     },
+			this.$refs.feedlist.children[this.$store.state.curFeedIndex].scrollIntoViewIfNeeded(false);
+    },
 		selectFeedByIndex(index) {
-       /*
-			 if (index >= this.fileNumber) {
-         this.currentIndex = this.fileNumber - 1;
-       } else if (index <= 0) {
-         this.currentIndex = 0;
-       } else {
-         this.currentIndex = index;
-       }
-
-       this.keepSelectVisible();
-
-       this.updatePreview();*/
-     },
+			var len = this.$store.state.feedsList.length;
+			if (index >= len) {
+				this.changeCurFeedByIndex(len - 1)
+			} else if (index <= 0) {
+				this.changeCurFeedByIndex(0)
+			} else {
+				this.changeCurFeedByIndex(index)
+			}
+			this.keepSelectVisible();
+		},
 		selectNextFeed() {
-			this.selectFileByIndex(this.currentIndex + 1);
+			this.selectFeedByIndex(this.$store.state.curFeedIndex + 1);
 		},
 		selectPrevFeed() {
-			this.selectFileByIndex(this.currentIndex - 1);
+			this.selectFeedByIndex(this.$store.state.curFeedIndex - 1);
 		}
 	}
 })
@@ -96,16 +116,32 @@ export default ({
 
 <style scoped>
 
-.mu-appbars-title {
+.content {
 	position: fixed;
 	width: 35%;
 	margin-top: 100px;
+
+  flex-direction: row;
+
+	flex: 1 1 0px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.mu-appbars-title {
+	width: 100%;
+	position: relative;
+	display: flex;
+	flex-direction: column;
 }
 
 .mu-appbars {
 	width: 100%;
 	position: relative;
+	display: flex;
 }
+
 .mu-appbar {
 	width: 100%;
 	position: relative;
@@ -114,6 +150,12 @@ export default ({
 .mu-appbars-button {
 	position: relative;
 	float: right;
+}
+
+.feeds-list {
+	width: 100%;
+	height: 75%;
+	overflow: scroll;
 }
 </style>
 
