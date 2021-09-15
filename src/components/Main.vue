12441c9695fc9091a026ebbf50af78dcaf81c038
shaochenheng <1010716589@qq.com>
2021-09-15 23:47:48 +0800

    vue-router/home.vue => Main.vue, use Main control components

---
+++ src/components/Main.vue
@@ -0,0 +1,141 @@
+<template>
+	<div class="home" id="home">
+    <div class="test-item">
+      <div class="button-list">
+        <button @click="selectPrevItem()">prev</button>
+        <button @click="selectNextItem()">next</button>
+        <button @click="openCurrentItem()">right</button>
+        <button @click="upItem()">left</button>
+      </div>
+      <check></check>
+    </div>
+		<feedsList 
+		v-if="!openArticle"
+		class="feedsList"
+		ref="feedslist" />
+
+		<articlesList 
+		v-if="curFeedIndex != -1 && !openArticle"
+		class="articlesList" 
+		ref="articlelist" />
+		
+		<articlePanel
+		v-if="openArticle && curArticleIndex != -1"
+		:article="$store.state.feedsList[curFeedIndex].feed_article_list[curArticleIndex]"/>
+		
+	</div>
+</template>
+
+<script>
+import articlesList from "@/components/articlesList.vue"
+import articlePanel from "@/components/articlePanel.vue"
+import feedsList from "@/components/feedsList.vue"
+import check from "@/components/check.vue"
+import {mapState, mapGetters} from 'vuex';
+
+export default {
+	name: "home",
+	data () {
+		return {
+		};
+	},
+	computed: {
+		...mapState([
+		'curFeedIndex', 
+		'curArticleIndex', 
+		'openFeed', 
+		'openArticle']),
+		...mapGetters(['curFeedArticleList']),
+	},
+	components:{
+		articlesList,
+		articlePanel,
+		feedsList,
+    check
+	},
+	mounted() {
+		window.selectNextItem = this.selectNextItem;
+		window.selectPrevItem = this.selectPrevItem;
+		window.openCurrentItem = this.openCurrentItem;
+		window.upItem = this.upItem;
+	},
+	methods: {
+		changeViewKey(key) {
+			this.$store.commit('changeViewKey', key)
+		},
+		articleInfoList(){
+			return this.curFeedArticleList.find(
+				article => article.title === this.title
+			)
+		},
+		selectNextItem() {
+			if (!this.openFeed && !this.openArticle) {
+				this.$refs.feedslist.selectFeedByIndex(this.curFeedIndex + 1);
+			} else if (this.openFeed && !this.openArticle) {
+				this.$refs.articlelist.selectArticleByIndex(this.curArticleIndex + 1);
+			} 
+		},
+		selectPrevItem() {
+			if (!this.openFeed && !this.openArticle) {
+				this.$refs.feedslist.selectFeedByIndex(this.curFeedIndex - 1);
+			} else if (this.openFeed && !this.openArticle) {
+				this.$refs.articlelist.selectArticleByIndex(this.curArticleIndex - 1);
+			}
+		},
+		openCurrentItem() {
+			if (!this.openFeed && !this.openArticle) {
+				if (this.curFeedIndex != -1 && this.curArticleIndex === -1) {
+					
+					this.$store.commit('changeOpenFeed', true);
+				}
+			} else if (this.openFeed && !this.openArticle) {
+				if (this.curFeedIndex != -1 && this.curArticleIndex != -1) {
+					this.$store.commit('changeOpenArticle', true);
+				}
+			}
+		},
+		upItem() {
+			if (this.openFeed && this.openArticle) {
+				if (this.curFeedIndex != -1 && this.curArticleIndex != -1) {
+					this.$store.commit('changeOpenArticle', false);
+				}
+			} else if (this.openFeed && !this.openArticle) {
+				if (this.curFeedIndex != -1) {
+					this.$store.commit('changeOpenFeed', false);
+					this.$store.commit('changeCurArticleIndex', -1);
+				}
+			} else if (!this.openFeed && !this.openArticle) {
+				if (this.curFeedIndex != -1) {
+					this.$store.commit('changeCurFeedIndex', -1);
+					this.$store.commit('changeCurArticleIndex', -1);
+				}
+			}
+		}
+	}
+};
+</script>
+
+<style scoped>
+
+.view-bar {
+	position: fixed;
+}
+#home{
+	max-width: 1400px;
+	margin:0 auto;
+
+	display: flex;
+  flex-direction: column;
+  
+  width: 100%;
+  height: 100%;
+
+  position: relative;
+}
+.test-item {
+  flex-direction: column;
+}
+.button-list {
+  flex-direction: row;
+}
+</style>
