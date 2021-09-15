adfc5e88cdf6e96ee7230f843849a4f4d3cb0d6c
shaochenheng <1010716589@qq.com>
2021-09-15 23:45:32 +0800

    remove vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import MuseUI from 'muse-ui';
 import 'muse-ui/dist/muse-ui.css';
 import store from './store'
@@ -9,7 +8,6 @@ Vue.use(MuseUI);
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount('#app')
