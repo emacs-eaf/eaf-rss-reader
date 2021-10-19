b6f3a57dae723338ad8e8dbdd90c6cd497f9da1e
Andy Stewart <lazycat.manatee@gmail.com>
2021-10-19 22:01:12 +0800

    Fix keybinding.

--- eaf-rss-reader.el
+++ eaf-rss-reader.el
@@ -3,17 +3,10 @@
   '(("A" . "add_feed")
     ("R" . "remove_feed")
     ("g" . "handle_refresh_rsshub_list")
-
-    ("1" . "js_select_prev_feed")
-    ("2" . "js_select_next_feed")
-    ("3" . "js_select_prev_article")
-    ("4" . "js_select_next_article")
-
-    ("p" . "js_select_prev_feed")
     ("n" . "js_select_next_feed")
-    ("j" . "js_select_prev_article")
-    ("k" . "js_select_next_article")
-
+    ("p" . "js_select_prev_feed")
+    ("j" . "js_select_next_article")
+    ("k" . "js_select_prev_article")
     ("<f12>" . "open_devtools")
     )
   "The keybinding of EAF RSS Reader."
